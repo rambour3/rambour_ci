d13a06657a27212cda7170c0ee321aeb73d39dad
thibaultrambour <thibaultrambour3@gmail.com>
2020-03-02 17:27:40 +0100

    first commit

---
+++ src/main/java/com/sysops/thibaultrambour/ThibaultrambourApplication.java
@@ -0,0 +1,13 @@
+package com.sysops.thibaultrambour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThibaultrambourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThibaultrambourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sysops/thibaultrambour/ThibaultrambourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysops.thibaultrambour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThibaultrambourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
